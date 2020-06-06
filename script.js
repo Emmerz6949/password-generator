10b37c363fdaff6b287c13ee526282fbe2ee93f1
emmerz6949 <emmerz6949@gmail.com>
2020-06-06 00:57:16 -0400

    added an if statement after every prompt to vallidate each answer by catching any invalid answers, and removed an alert and comment as they were unnecessary

--- script.js
+++ script.js
@@ -16,16 +16,42 @@ var randNum = Math.floor(Math.random() * 10) ;
 var special = [' ', '!', '#', '$', '%', '&', '*', '+', '-', '.', '/', ':', ';', '?', '@', '^', '_', '~'];
 
 function generatePassword() {
-  //prompt will = null if they cancel, confirm is true or false
-  alert('Looks like you need a new password!')
   
-  var passLength = prompt('How long would you your password to be? (Choose a number between 8 and 128)');
+  var passLength = parseInt(prompt('How long would you your password to be? (Choose a number between 8 and 128)'));
+  if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+    alert('Invalid input! Press the "Generate Password" button and try again.');
+    return document.querySelector("#password").value;
+  }
+  
   var lowL = prompt('Would you like your password to contain lowercase letters? (Reply y for yes and n for no)');
-  var upL = prompt('Would you like your password to contain uppercase letters? (Reply y for yes and n for no)');
-  var ranNum = prompt('Would you like your password to contain numbers? (Reply y for yes and n for no)');
-  var sChar = prompt('Would you like your password to contain special characters? (Reply y for yes and n for no)');
+  if ((lowL == null) || (lowL.toLowerCase() !== 'y' && lowL.toLowerCase() !== 'n')) {
+    alert('Invalid input! Press the "Generate Password" button and try again.');
+    return document.querySelector("#password").value;
+  }
 
+  var upL = prompt('Would you like your password to contain uppercase letters? (Reply y for yes and n for no)');
+  if ((upL == null) || (upL.toLowerCase() !== 'y' && upL.toLowerCase() !== 'n')) {
+    alert('Invalid input! Press the "Generate Password" button and try again.');
+    return document.querySelector("#password").value;
+  }
+
+  var wantNum = prompt('Would you like your password to contain numbers? (Reply y for yes and n for no)');
+  if ((wantNum == null) || (wantNum.toLowerCase() !== 'y' && wantNum.toLowerCase() !== 'n')) {
+    alert('Invalid input! Press the "Generate Password" button and try again.');
+    return document.querySelector("#password").value;
+  }
+
+  var sChar = prompt('Would you like your password to contain special characters? (Reply y for yes and n for no)');
+  if ((sChar == null) || (sChar.toLowerCase() !== 'y' && sChar.toLowerCase() !== 'n')) {
+    alert('Invalid input! Press the "Generate Password" button and try again.');
+    return document.querySelector("#password").value;
+  }
   
+
 }
+
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
